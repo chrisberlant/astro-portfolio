48612e6eaa56ca19aa58cf9573e6ee5721d59362
Chris Berlant <chris.berlant@gmail.com>
2024-11-21 16:34:34 +0100

    feat: gh-pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://chrisberlant.github.io",
+    base: "astro-portfolio",
     integrations: [
         tailwind({
             applyBaseStyles: false,
